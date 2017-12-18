603343fd270c50da5289c4c12fbd2ac1070cc303
julyinsung <julyinsung@gmail.com>
2017-12-18 15:03:49 +0900

    수정

--- jenkins_test/Jenkinsfile.groovy
+++ jenkins_test/Jenkinsfile.groovy
@@ -17,7 +17,7 @@ node {
         // Get the Maven tool.
         // ** NOTE: This 'M3' Maven tool must be configured
         // **       in the global configuration.
-        mvnHome = tool 'M3'
+        mvnHome = tool 'maven'
     }
     stage('Checkout') {
         // Get some code from a Git repository
