ef1ece0e70d311c8abb73f6761d1d5a77df7e62e
julyinsung <julyinsung@gmail.com>
2017-12-19 13:34:34 +0900

    tag 추가 방식 변경

--- jenkins_test/Jenkinsfile.groovy
+++ jenkins_test/Jenkinsfile.groovy
@@ -2,7 +2,10 @@ def mvnHome
 node {
    stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
-      git url: 'https://github.com/julyinsung/jenkins_test.git', tag: 'release_1.0' 
+      //git url: 'https://github.com/julyinsung/jenkins_test.git', tag: 'release_1.0'
+      checkout([$class: 'GitSCM', branches: [[name: 'refs/tags/release_1.0']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/julyinsung/jenkins_test.git']]])
+      
+       
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
       // **       in the global configuration.           
