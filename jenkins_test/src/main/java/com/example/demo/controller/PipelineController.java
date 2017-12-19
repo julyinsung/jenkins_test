3a46e051bb03324d507132bf0b990886ed3c70bf
julyinsung <julyinsung@gmail.com>
2017-12-19 11:11:56 +0900

    hello 페이지 추가

--- jenkins_test/src/main/java/com/example/demo/controller/PipelineController.java
+++ jenkins_test/src/main/java/com/example/demo/controller/PipelineController.java
@@ -1,9 +1,24 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.domain.User;
 
 @Controller
 public class PipelineController {
 
-	
+
+	@RequestMapping("/")
+	public String hello(@RequestParam(value="name", required=false, defaultValue="Worlds") String name
+			,Model model){
+
+		User user = new User();
+		user.setName("mike");
+		model.addAttribute("user", user);
+		return "hello/hello";
+		
+	}
 }
---
+++ jenkins_test/src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain;
+
+public class User {
+
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
